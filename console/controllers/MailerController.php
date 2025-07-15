c19db88cfbbb6b1ad8e2de2c8ce524872020a52c
AnuchkinAC <uniqcle@yandex.ru>
2025-07-15 16:51:45 +0700

    Update MailerController.php

--- console/controllers/MailerController.php
+++ console/controllers/MailerController.php
@@ -15,8 +15,8 @@ class MailerController extends \yii\console\Controller
 
 		//Работает только в тестовом режиме. реально письма не отправляет, а помещает в папку runtime/mail
 		$result = Yii::$app->mailer->compose()
-                ->setFrom('uniqcle@yandex.ru')
-                ->setTo('develop2207.nsk@gmail.com')
+                ->setFrom('')
+                ->setTo('')
                 ->setSubject('Тестовый заголовок письма')
                 ->setTextBody('Тестовое сообщение')
                 ->setHtmlBody('<b>текст сообщения в формате HTML</b>')
